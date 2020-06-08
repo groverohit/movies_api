290c72f682f6a230462b29bcdc804f4a1d70133f
groverohit <groverohit@gmail.com>
2020-06-08 10:31:43 -0700

    Set up for hosting on Heroku

--- index.js
+++ index.js
@@ -237,7 +237,11 @@ app.delete(
   }
 );
 
-app.listen(8080, () => console.log('Your app is listening on port 8080.'));
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0', () => {
+  console.log('Listening on port ' + port);
+});
+//app.listen(8080, () => console.log('Your app is listening on port 8080.'));
 
 app.use(express.static('public'));
 
